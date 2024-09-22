e7e315f330e1aba0ab7bf132e5cc950683b595f2
Llorens19 <llorenssorianodiego@gmail.com>
2024-09-22 12:19:55 +0200

    Implementer plantilla a la vista del home

--- Frontend/src/app/shared/components/carousel-items/carousel-items.component.ts
+++ Frontend/src/app/shared/components/carousel-items/carousel-items.component.ts
@@ -1,28 +1,32 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CarouselDetails, CarouselHome } from '../../models';
 import { CommonModule } from '@angular/common';
+import { NgbModule, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 
 
 @Component({
   selector: 'app-carousel-items',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, NgbModule, NgbCarouselModule],
   templateUrl: './carousel-items.component.html',
   styleUrl: './carousel-items.component.css'
 })
 export class CarouselItemsComponent implements OnInit {
 
-
   @Input() categories!: CarouselHome[];
   @Input() products_details!: CarouselDetails[];
   @Input() page!: String;
-
 
   selectIndex = 0;
   selectIndex_product_img = 0;
 
   constructor() { }
+
   ngOnInit(): void {
+    console.log('CarouselItemsComponent initialized with categories:', this.categories);
+    console.log('CarouselItemsComponent initialized with products_details:', this.products_details);
+    console.log('CarouselItemsComponent initialized with page:', this.page);
   }
 }
